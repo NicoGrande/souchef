687f8a26d5f4f78833fee5a980f57c5ee851af2d
Nicolas Grande <nicolas.grande@me.com>
2024-10-25 22:48:35 -0700

    adding extract items agent.

---
+++ backend/src/server/extract_items.py
@@ -0,0 +1,190 @@
+import dotenv
+from langchain.agents import create_openai_tools_agent, AgentExecutor
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+from langchain_community.tools import Tool
+from langchain_google_community import GoogleSearchAPIWrapper
+from langchain.output_parsers import PydanticOutputParser
+import pprint
+import httpx
+import base64
+
+dotenv.load_dotenv()
+
+
+class ExtractItemsAgent:
+    """
+    Extracts items from a receipt and an image of the items on the receipt.
+
+    This class uses a language model and various tools to process receipt information
+    and extract detailed item data, including nutritional information.
+
+    Attributes:
+        _llm: The language model used for processing.
+        _extract_items_prompt: The prompt template for item extraction.
+        _tools: A list of tools available to the agent.
+        _extract_items_agent: The agent responsible for item extraction.
+        _extract_items_agent_executor: The executor for the extraction agent.
+        _schema: The schema definition for the output data.
+        _branded_items_url: URL for branded items database.
+        _foundation_items_url: URL for foundation items database.
+    """
+
+    def __init__(
+        self, model: str = "gpt-4o-mini", temperature: float = 0, verbose: bool = False
+    ):
+        """
+        Initializes the ExtractItemsAgent.
+
+        Args:
+            model (str): The name of the language model to use. Defaults to "gpt-4o-mini".
+            temperature (float): The temperature setting for the language model. Defaults to 0.
+            verbose (bool): Whether to enable verbose output. Defaults to False.
+        """
+        self._llm = ChatOpenAI(model=model, temperature=temperature)
+        self._extract_items_prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    "You are recieving a receipt and an image of the items on the receipt for a particular supermarket purchase.",
+                ),
+                (
+                    "system",
+                    "Use the receipt and the following image to extract items. The receipt may be blurry, so use the image to help you. There should be one item per line on the receipt.",
+                ),
+                (
+                    "system",
+                    "Items shoudld extracted with the following schema: {schema}.",
+                ),
+                (
+                    "system",
+                    "Be sure to extract the supermarket or merchat from the top of the receipt, as well as the date of the purchase.",
+                ),
+                (
+                    "system",
+                    "If required, extract any necessary information about each item, such as the quantity, serving size and nutrition information from the merchat website.",
+                ),
+                (
+                    "system",
+                    "If required, extract any necessary information about branded items, such as the quantity, serving size and nutrition information from {branded_items_url}.",
+                ),
+                (
+                    "system",
+                    "If required, extract any necessary information about foundational items, such as the quantity, serving size and nutrition information from {foundation_items_url}.",
+                ),
+                (
+                    "system",
+                    "Output your final answer in JSON format.",
+                ),
+                (
+                    "human",
+                    [
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": "data:image/jpeg;base64,{receipt_data}"
+                            },
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {"url": "data:image/jpeg;base64,{image_data}"},
+                        },
+                    ],
+                ),
+                MessagesPlaceholder(variable_name="agent_scratchpad"),
+            ]
+        )
+        self._tools = []
+        self._setup_tools()
+        self._extract_items_agent = create_openai_tools_agent(
+            self._llm, self._tools, prompt=self._extract_items_prompt
+        )
+        self._extract_items_agent_executor = AgentExecutor(
+            agent=self._extract_items_agent, tools=self._tools, verbose=verbose
+        )
+        self._schema = """
+            Receipt:
+                merchant: str
+                date: str
+                items: list[Item]
+
+            Item:
+                name: str
+                brand: str
+                price: float
+                quantity: float
+                unit: str
+                serving_size: str
+                nutrition_info: dict[Macro, quantity]
+                shelf_life: str
+            
+            Macro:
+                calories: float
+                protein: float
+                fat: float
+                carbohydrates: float
+                sugar: float
+                fiber: float
+        """
+        self._branded_items_url = (
+            "https://fdc.nal.usda.gov/fdc-app.html#/food-search?query=&type=Branded"
+        )
+        self._foundation_items_url = (
+            "https://fdc.nal.usda.gov/fdc-app.html#/food-search?query=&type=Foundation"
+        )
+
+    def _setup_tools(self):
+        """
+        Sets up the tools used by the agent.
+
+        This method initializes the Google Search API tool and adds it to the agent's toolkit.
+        """
+        google_search_api = GoogleSearchAPIWrapper()
+        self._tools.append(
+            Tool(
+                name="google_search",
+                description="Search the internet for nutritional and serving size information of food items.",
+                func=google_search_api.run,
+            )
+        )
+
+    def extract_items_from_receipt(self, receipt_url: str, image_url: str) -> list[str]:
+        """
+        Extracts items from a receipt and an image of the items.
+
+        This method processes the receipt and image data, and uses the agent to extract
+        detailed information about the items.
+
+        Args:
+            receipt_url (str): URL of the receipt image.
+            image_url (str): URL of the image containing the items.
+
+        Returns:
+            list[str]: A list of extracted items with their details.
+        """
+        receipt_data = base64.b64encode(httpx.get(receipt_url).content).decode("utf-8")
+        image_data = base64.b64encode(httpx.get(image_url).content).decode("utf-8")
+
+        return self._extract_items_agent_executor.invoke(
+            {
+                "receipt_data": receipt_data,
+                "image_data": image_data,
+                "schema": self._schema,
+                "branded_items_url": self._branded_items_url,
+                "foundation_items_url": self._foundation_items_url,
+            }
+        )
+
+
+def main():
+    agent = ExtractItemsAgent()
+    pprint.pp(
+        agent.extract_items_from_receipt(
+            receipt_url="https://i.redd.it/4r55c4845kvc1.jpg",
+            image_url="https://i.redd.it/5dmd65845kvc1.jpg",
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
